27a09b7d791d9be5430eaf3fba162176b962eb06
Oleksandr <oleksandr.suhak@grammarly.com>
2022-01-22 18:45:19 +0200

    canwas, drawing a cell

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello from ts!')
+console.log('Game of Life in TS + fp-ts!')
+
+const canvas = document.querySelector<HTMLCanvasElement>("#game")
+
+const winWidth = window.innerWidth
+const winHeight = window.innerHeight
+canvas.width = winWidth
+canvas.height = winHeight
+
+const ctx = canvas.getContext("2d")
+ctx.fillStyle = "rgb(100, 240, 150)"
+
+const cellSize = 10
+const cellsY = winWidth / cellSize
+const cellsX = winHeight / cellSize
+
+function drawCell(x: number, y: number) {
+  ctx.fillRect(x, y, cellSize, cellSize)
+}
+
+drawCell(20, 20)
